37107cb557ac90a0de7600664236f986a49677e6
Stuart Pittaway <1201909+stuartpittaway@users.noreply.github.com>
2021-02-22 15:09:13 +0000

    LM5009 design and datasheets

--- GenerateJLCPCBPARTLIST.cmd
+++ GenerateJLCPCBPARTLIST.cmd
@@ -1,5 +1,5 @@
 e:
 cd E:\source\JLCKicadTools
 SET PYTHONPATH=.
-C:\Users\Stuart\AppData\Local\Programs\Python\Python38-32\python.exe E:\source\JLCKicadTools\jlc_kicad_tools\generate_jlc_files.py --warn-no-lcsc-partnumber --database=E:\source\diyBMS-CurrentShunt\diyBMS-CurrentShunt\cpl_rotations_db.csv --verbose E:\source\diyBMS-CurrentShunt\diyBMS-CurrentShunt
+C:\Users\Stuart\AppData\Local\Programs\Python\Python38-32\python.exe E:\source\JLCKicadTools\jlc_kicad_tools\generate_jlc_files.py --warn-no-lcsc-partnumber --database=E:\source\JLCKicadTools\jlc_kicad_tools\cpl_rotations_db.csv --verbose E:\source\diyBMS-CurrentShunt\CurrentShuntCircuit
 pause
